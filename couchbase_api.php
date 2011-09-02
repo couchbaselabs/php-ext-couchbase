27ccc6700748db965159c97675fd16c3c576c56e
Jan Lehnardt <jan@apache.org>
2011-09-02 10:18:57 +0200

    closely mirror the libcouchbase API

--- couchbase_api.php
+++ couchbase_api.php
@@ -10,25 +10,36 @@
 $couchbase_handle = couchbase_create($host, $user, $pass, $bucket);
 $success = couchbase_connect($couchbase_handle);
 
-$success = couchbase_get($couchbase_handle, $key_s, $callback, $expiry = null);
-$success = couchbase_get_by_key($couchbase_handle, $hash, $key_s, $callback, $expiry = null);
+$success = couchbase_set_get_callback($couchbase_handle, $callback);
+$success = couchbase_set_storage_callback($couchbase_handle, $callback);
+$success = couchbase_set_remove_callback($couchbase_handle, $callback);
+$success = couchbase_set_arithmetic_callback($couchbase_handle, $callback);
+$success = couchbase_set_touch_callback($couchbase_handle, $callback);
 
-$success = couchbase_set($couchbase_handle, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
-$success = couchbase_add($couchbase_handle, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
-$success = couchbase_replace($couchbase_handle, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_set_tap_mutation_callback($couchbase_handle, $callback);
+$success = couchbase_set_tap_deletion_callback($couchbase_handle, $callback);
+$success = couchbase_set_tap_opaque_callback($couchbase_handle, $callback);
+$success = couchbase_set_tap_vbucket_set_callback($couchbase_handle, $callback);
 
-$success = couchbase_set_by_key($couchbase_handle, $hash, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
-$success = couchbase_add_by_key($couchbase_handle, $hash, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
-$success = couchbase_replace_by_key($couchbase_handle, $hash, $key, $value, $callback, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_get($couchbase_handle, $key_s, $expiry = null);
+$success = couchbase_get_by_key($couchbase_handle, $hash, $key_s, $expiry = null);
 
-$success = couchbase_touch($couchbase_handle, $key_s, $expiry, $callback);
-$success = couchbase_touch_by_key($couchbase_handle, $hash, $key_s, $expiry, $callback);
+$success = couchbase_set($couchbase_handle, $key, $value, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_add($couchbase_handle, $key, $value, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_replace($couchbase_handle, $key, $value, $flags = null, $expiry = null, $cas = null);
 
-$success = couchbase_arithmetic($couchbase_handle, $key, $delta, $create, $initial, $callback); // useful for incr/decr etc
-$success = couchbase_arithmetic_by_key($couchbase_handle, $hash, $key, $delta, $create, $initial, $callback); // useful for incr/decr etc
+$success = couchbase_set_by_key($couchbase_handle, $hash, $key, $value, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_add_by_key($couchbase_handle, $hash, $key, $value, $flags = null, $expiry = null, $cas = null);
+$success = couchbase_replace_by_key($couchbase_handle, $hash, $key, $value, $flags = null, $expiry = null, $cas = null);
 
-$success = couchbase_remove($couchbase_handle, $key, $callback, $cas = null);
-$success = couchbase_remove_by_key($couchbase_handle, $hash, $key, $callback, $cas = null);
+$success = couchbase_touch($couchbase_handle, $key_s, $expiry);
+$success = couchbase_touch_by_key($couchbase_handle, $hash, $key_s, $expiry);
+
+$success = couchbase_arithmetic($couchbase_handle, $key, $delta, $create, $initial); // useful for incr/decr etc
+$success = couchbase_arithmetic_by_key($couchbase_handle, $hash, $key, $delta, $create, $initial); // useful for incr/decr etc
+
+$success = couchbase_remove($couchbase_handle, $key, $cas = null);
+$success = couchbase_remove_by_key($couchbase_handle, $hash, $key, $cas = null);
 
 $success = couchbase_destroy($couchbase_handle);
 
