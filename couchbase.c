434b52ca7ae86612255b8f5779f0991a59bd2c58
Michael Orion Jackson <mjackson@omniti.com>
2012-09-11 16:03:43 -0400

    Removing 'expandtab' from vim modeline
    Per discussion in #libcouchbase, removing expandtab from the vim
    modeline.  Inserting literal tabs (i.e. via the "noet" setting
    here) is the desired state to comply with PECL coding standards.
    
    Change-Id: Ia87e78bc9274142bf7f7102f75445f2bbff9dabd
    Reviewed-on: http://review.couchbase.org/20746
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Mordechai Nunberg <mnunberg@haskalah.org>
    Tested-by: Mordechai Nunberg <mnunberg@haskalah.org>
    Reviewed-on: http://review.couchbase.org/20872
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- couchbase.c
+++ couchbase.c
@@ -3457,6 +3457,6 @@ PHP_MINFO_FUNCTION(couchbase)
  * tab-width: 4
  * c-basic-offset: 4
  * End:
- * vim600: noet expandtab sw=4 ts=4 fdm=marker
+ * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: noet sw=4 ts=4
  */
