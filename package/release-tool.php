93bfd8b6770ab3fdbe7cdd966379d9b9213747a1
Matt Ingenthron <ingenthr@cep.net>
2012-10-22 15:29:24 -0700

    Update metadata for 1.1.0-dp5 release.
    Change-Id: Ie3638591923d03ab8f855c0c0b7fea6dff59ab3b
    Reviewed-on: http://review.couchbase.org/21854
    Reviewed-by: Michael Jackson <mjackson@omniti.com>
    Tested-by: Mordechai Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Mordechai Nunberg <mnunberg@haskalah.org>

--- package/release-tool.php
+++ package/release-tool.php
@@ -2,7 +2,7 @@
 <?php
 
 $VERSIONS = array(
-	"1.1.0-dp4" => "php-ext-couchbase",
+	"1.1.0-dp5" => "php-ext-couchbase",
 	"1.0.5" => "php-ext-couchbase-1.0");
 $OSes = array("centos55", "centos62", "ubuntu1004", "ubuntu1110");
 $BITs = array("32" => "i686","64" => "x86_64");
