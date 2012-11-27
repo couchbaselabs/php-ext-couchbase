5d40a2b902db8f6c49c132246bc3ae1ec2b7ab1c
Trond Norbye <trond.norbye@gmail.com>
2012-11-27 11:13:10 +0100

    Simplify arithmetic implementation
    Change-Id: Iab30b3f722afe93260394ac4cfec0a6354f55eaf
    Reviewed-on: http://review.couchbase.org/22846
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- arithmetic.c
+++ arithmetic.c
@@ -1,93 +1,94 @@
 #include "internal.h"
 
+struct arithmetic_cookie {
+	lcb_error_t error;
+	uint64_t value;
+};
+
 /* {{{ static void php_couchbase_arithmetic_callback(...) */
-static void
-php_couchbase_arithmetic_callback(lcb_t instance,
-								  const void *cookie,
-								  lcb_error_t error,
-								  const lcb_arithmetic_resp_t *resp)
+static void php_couchbase_arithmetic_callback(lcb_t instance,
+											  const void *cookie,
+											  lcb_error_t error,
+											  const lcb_arithmetic_resp_t *resp)
 {
-	php_couchbase_ctx *ctx = (php_couchbase_ctx *)cookie;
-	php_ignore_value(instance);
-	if (--ctx->res->seqno == 0) {
-		pcbc_stop_loop(ctx->res);
-	}
-
-	ctx->res->rc = error;
-	if (LCB_SUCCESS != error) {
-		return;
-	}
-
+	struct arithmetic_cookie *c = (struct arithmetic_cookie *)cookie;
+	c->error = error;
 	if (resp->version != 0) {
-		ctx->res->rc = LCB_ERROR;
-		return;
+		c->error = LCB_ERROR;
+	} else {
+		c->value = resp->v.v0.value;
 	}
-
-	ZVAL_LONG(ctx->rv,	resp->v.v0.value);
 }
-/* }}} */
 
 PHP_COUCHBASE_LOCAL
 void php_couchbase_arithmetic_impl(INTERNAL_FUNCTION_PARAMETERS, char op, int oo) /* {{{ */
 {
 	char *key;
-	time_t exp = {0};
+	time_t exp;
 	long klen = 0, offset = 1, expire = 0;
 	long create = 0, initial = 0;
 	php_couchbase_res *couchbase_res;
-	int argflags = oo ? PHP_COUCHBASE_ARG_F_OO : PHP_COUCHBASE_ARG_F_FUNCTIONAL;
+	lcb_arithmetic_cmd_t cmd;
+	const lcb_arithmetic_cmd_t *const commands[] = { &cmd };
+	lcb_error_t retval;
+	struct arithmetic_cookie cookie;
+	int argflags;
+	long delta;
+	lcb_t instance;
 
+	argflags = oo ? PHP_COUCHBASE_ARG_F_OO : PHP_COUCHBASE_ARG_F_FUNCTIONAL;
 	PHP_COUCHBASE_GET_PARAMS(couchbase_res, argflags,
-							 "s|llll", &key, &klen, &offset, &create, &expire, &initial);
+							 "s|llll", &key, &klen, &offset, &create,
+							 &expire, &initial);
+	instance = couchbase_res->handle;
 
-	{
-		lcb_error_t retval;
-		php_couchbase_ctx *ctx;
-		long delta = (op == '+') ? offset : -offset;
+	memset(&cookie, 0, sizeof(cookie));
+	delta = (op == '+') ? offset : -offset;
 
-		if (expire) {
-			exp = pcbc_check_expiry(expire);
-		}
-
-		ctx = ecalloc(1, sizeof(php_couchbase_ctx));
-		ctx->res = couchbase_res;
-		ctx->rv = return_value;
-
-		{
-			lcb_arithmetic_cmd_t cmd;
-			lcb_arithmetic_cmd_t *commands[] = { &cmd };
-			memset(&cmd, 0, sizeof(cmd));
-			cmd.v.v0.key = key;
-			cmd.v.v0.nkey = klen;
-			cmd.v.v0.create = create;
-			cmd.v.v0.delta = delta;
-			cmd.v.v0.initial = initial;
-			cmd.v.v0.exptime = exp;
-
-			retval = lcb_arithmetic(couchbase_res->handle, ctx, 1,
-									(const lcb_arithmetic_cmd_t * const *)commands);
-		}
-		if (LCB_SUCCESS != retval) {
-			efree(ctx);
-			php_error_docref(NULL TSRMLS_CC, E_WARNING,
-							 "Failed to schedule rithmetic request: %s", lcb_strerror(couchbase_res->handle, retval));
-			RETURN_FALSE;
-		}
-
-		couchbase_res->seqno += 1;
-		pcbc_start_loop(couchbase_res);
-		if (LCB_SUCCESS != ctx->res->rc) {
-			// Just return false and don't print a warning when no key is present and create is false
-			if (!(LCB_KEY_ENOENT == ctx->res->rc && create == 0)) {
-				php_error_docref(NULL TSRMLS_CC, E_WARNING,
-								 "Failed to %s value in server: %s", (op == '+') ? "increment" : "decrement", lcb_strerror(couchbase_res->handle, ctx->res->rc));
-			}
-			efree(ctx);
-			RETURN_FALSE;
-		}
-
-		efree(ctx);
+	if (expire) {
+		exp = pcbc_check_expiry(expire);
+	} else {
+		exp = 0;
 	}
+
+	memset(&cmd, 0, sizeof(cmd));
+	cmd.v.v0.key = key;
+	cmd.v.v0.nkey = klen;
+	cmd.v.v0.create = create;
+	cmd.v.v0.delta = delta;
+	cmd.v.v0.initial = initial;
+	cmd.v.v0.exptime = exp;
+
+	lcb_behavior_set_syncmode(instance, LCB_SYNCHRONOUS);
+	retval = lcb_arithmetic(instance, &cookie, 1, commands);
+	lcb_behavior_set_syncmode(instance, LCB_ASYNCHRONOUS);
+
+	if (retval != LCB_SUCCESS || cookie.error != LCB_SUCCESS) {
+		char errmsg[256];
+		if (cookie.error == LCB_KEY_ENOENT && create == 0) {
+			/* The user told us to not create the key... */
+			RETURN_FALSE;
+		}
+
+		if (retval == LCB_SUCCESS) {
+			retval = cookie.error ;
+		}
+		couchbase_res->rc = retval;
+
+		snprintf(errmsg, sizeof(errmsg), "Failed to %s value in the server: %s",
+				 (op == '+') ? "increment" : "decrement",
+				 lcb_strerror(instance, retval));
+		if (oo) {
+			zend_throw_exception(cb_lcb_exception, errmsg, 0 TSRMLS_CC);
+			return;
+		} else {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, errmsg);
+			RETURN_FALSE;
+		}
+	}
+
+	couchbase_res->rc = retval;
+	ZVAL_LONG(return_value, cookie.value);
 }
 /* }}} */
 
