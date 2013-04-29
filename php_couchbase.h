8715fa955f917a851a923fa45edd002294d1fa14
Pierre Joye <pierre.php@gmail.com>
2013-04-29 20:15:10 +0200

    prepare 1.1.5
    Change-Id: I2ad3286a7691a0c3546cb7d777e6645f9ade65fe
    Reviewed-on: http://review.couchbase.org/26037
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- php_couchbase.h
+++ php_couchbase.h
@@ -20,7 +20,7 @@
 #ifndef PHP_COUCHBASE_H
 #define PHP_COUCHBASE_H
 
-#define PHP_COUCHBASE_VERSION_STRING "1.1.5-dev"
+#define PHP_COUCHBASE_VERSION_STRING "1.1.5"
 
 extern zend_module_entry couchbase_module_entry;
 #define phpext_couchbase_ptr &couchbase_module_entry
