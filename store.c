afe52004e33d9f1ec4e903ea77a2d6bcf03bc096
Trond Norbye <trond.norbye@gmail.com>
2013-05-09 12:47:57 +0200

    Revert "PCBC-227: Allow store of pure numeric keys"
    This patch breaks PCBC-191
    
    This reverts commit 8382d3321bc09d8a5ecdba2c3726f65998ba2664.
    
    Change-Id: Iafd740c6594d9cb31654226cfafb938e3728a2e4
    Reviewed-on: http://review.couchbase.org/26223
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- store.c
+++ store.c
@@ -669,20 +669,8 @@ void php_couchbase_store_multi_impl_oo(INTERNAL_FUNCTION_PARAMETERS)
 		char *payload;
 		unsigned int flags = 0;
 		zval **ppzval;
-		int allocated_key = 0;
 
-		switch (zend_hash_get_current_key_type(Z_ARRVAL_P(akeys))) {
-		case HASH_KEY_IS_LONG: {
-			ulong kval;
-			zend_hash_get_current_key(Z_ARRVAL_P(akeys), &key, &kval, 0);
-			spprintf(&key, 0, "%llu", kval);
-			allocated_key = 1;
-		}
-		break;
-		case HASH_KEY_IS_STRING:
-			zend_hash_get_current_key(Z_ARRVAL_P(akeys), &key, NULL, 0);
-			break;
-		default: {
+		if (zend_hash_get_current_key_type(Z_ARRVAL_P(akeys)) != HASH_KEY_IS_STRING) {
 			int xx;
 			for (xx = 0; xx < ii; ++xx) {
 				efree((void *)cmds[xx].v.v0.bytes);
@@ -697,10 +685,9 @@ void php_couchbase_store_multi_impl_oo(INTERNAL_FUNCTION_PARAMETERS)
 								 0 TSRMLS_CC);
 			return ;
 		}
-		}
 
-		klen = strlen(key);
-		if (klen == 0) {
+		zend_hash_get_current_key(Z_ARRVAL_P(akeys), &key, NULL, 0);
+		if ((klen = strlen(key)) == 0) {
 			int xx;
 			for (xx = 0; xx < ii; ++xx) {
 				efree((void *)cmds[xx].v.v0.bytes);
@@ -745,9 +732,7 @@ void php_couchbase_store_multi_impl_oo(INTERNAL_FUNCTION_PARAMETERS)
 
 		if (couchbase_res->prefix_key_len) {
 			char *new_key;
-			allocated_key = 1;
-			klen = spprintf(&new_key, 0, "%s_%s", couchbase_res->prefix_key,
-							key);
+			klen = spprintf(&new_key, 0, "%s_%s", couchbase_res->prefix_key, key);
 			key = new_key;
 		}
 
@@ -762,7 +747,7 @@ void php_couchbase_store_multi_impl_oo(INTERNAL_FUNCTION_PARAMETERS)
 		cmds[ii].v.v0.flags = flags;
 		cmds[ii].v.v0.exptime = exp;
 
-		if (allocated_key) {
+		if (couchbase_res->prefix_key_len) {
 			efree(key);
 		}
 	}
