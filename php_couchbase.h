325acac5b46290998e63f6a2a0409a1aafc1707d
Trond Norbye <trond.norbye@gmail.com>
2013-09-26 11:40:08 +0200

    Next version should be 1.2.0
    We've extended the API a bit
    
    Change-Id: I96a00f1106bbac8948dfd318984f1b1a09485fb2
    Reviewed-on: http://review.couchbase.org/29204
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- php_couchbase.h
+++ php_couchbase.h
@@ -18,7 +18,7 @@
 #ifndef PHP_COUCHBASE_H
 #define PHP_COUCHBASE_H
 
-#define PHP_COUCHBASE_VERSION_STRING "1.1.5"
+#define PHP_COUCHBASE_VERSION_STRING "1.2.0"
 
 extern zend_module_entry couchbase_module_entry;
 #define phpext_couchbase_ptr &couchbase_module_entry
